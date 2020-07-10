a4fda8ed2eefa2688981477af972e91e5b9ce4bc
Markus Bawidamann <markustheswiss@gmail.com>
2020-07-10 00:09:41 -0700

    Version 0.1a: Initial code cleanup

--- mvng.py
+++ mvng.py
@@ -12,7 +12,7 @@ import signal
 
 def signal_handler(signal, frame):
 
-	print "Interrupted/terminated"
+	print("Interrupted/terminated")
 
 	sys.exit(0)
 
@@ -24,19 +24,21 @@ parser = optparse.OptionParser()
 
 parser.add_option('-f', '--force', 	action="store_false", dest="force_flag", help="Don't ask any stupid question, just do it! Default is off", default=False)
 #parser.add_option("-p", "--port", type="int", action="store", dest="port", help="Which port to connect for the port ping. Default is 22", default="22")
-parser.usage="%prog [options] Moves a file from source to destination \nWritten by Markus Bawidamann D20200709 after having an unhappy run in with legacy the legacy mv command."
+parser.usage="%prog [options] Source Destination\nMoves a file from source to destination \nWritten by Markus Bawidamann D20200709 after having an unhappy run in with the legacy mv command."
 
 
 options, args = parser.parse_args()
 
 
 if args == []:
-	print "You must at least specify a source and destination"
+	print("You must at least specify a source and destination")
 	parser.print_help()
 	sys.exit(1)
 
 
 
-interval = options.interval				#interval, run the connect test every x seconds
+force_flag = options.force_flag
 source=args[0]
 destination=args[1]
+
+print(source,destination)
